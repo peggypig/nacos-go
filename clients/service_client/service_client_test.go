075271fee285d82809de761149040f3aa4930ff9
codezhang <zjhfyq@vip.qq.com>
2019-01-09 13:50:45 +0800

    完成service_client.ModifyServiceInstance

--- clients/service_client/service_client_test.go
+++ clients/service_client/service_client_test.go
@@ -39,6 +39,30 @@ func TestServiceClient_RegisterServiceInstance(t *testing.T) {
 	}
 }
 
+func TestServiceClient_ModifyServiceInstance(t *testing.T) {
+	client := ServiceClient{
+		ServerConfigs: []constant.ServerConfig{constant.ServerConfig{
+			IpAddr: "10.0.0.8",
+			Port:   8848,
+		}},
+		ClientConfig: constant.ClientConfig{
+			TimeoutMs: 30 * 1000,
+		},
+	}
+	success, err := client.RegisterServiceInstance(vo.RegisterServiceInstanceParam{
+		Ip:          "10.0.0.10",
+		Port:        8848,
+		ServiceName: "demoservice",
+		Weight:      2,
+		ClusterName: "a",
+	})
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log(success)
+	}
+}
+
 func TestServiceClient_GetService(t *testing.T) {
 	client := ServiceClient{
 		ServerConfigs: []constant.ServerConfig{constant.ServerConfig{
